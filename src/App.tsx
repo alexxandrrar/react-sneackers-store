567e1833d356ecd373f6b7629be364dbb5a9ed61
alexxandrrar <alexandroovnaa@gmail.com>
2023-01-08 21:11:36 +0200

    feature/ card component

--- src/App.tsx
+++ src/App.tsx
@@ -14,4 +14,11 @@ function App() {
   )
 }
 
+// <Card
+//   id={1}
+//   title={'Nike Crater Impact Sneakers'}
+//   price={54654}
+//   imageUrl='https://cdn.runrepeat.com/i/nike/25573/nike-men-s-air-zoom-structure-21-running-shoes-blue-navy-white-7-5-d-m-us-mens-blue-navy-white-9760-main.jpg'
+// />
+
 export default App
