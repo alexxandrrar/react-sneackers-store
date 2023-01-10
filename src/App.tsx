a3f477b1ea7ae37a82a0b7e761c15018a0719ab0
alexxandrrar <alexandroovnaa@gmail.com>
2023-01-11 00:03:22 +0200

    feature/ main layout improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Header } from 'components/Header/Header'
-import { CarouselComponent } from 'components/CarouselComponent/CarouselComponent'
+import { Header } from './components/Header/Header'
+import { CarouselComponent } from './components/CarouselComponent/CarouselComponent'
+import { MainLayout } from './pages/MainLayout/MainLayout'
 
 import './App.css'
-import { MainLayout } from 'pages/MainLayout/MainLayout'
 
 function App() {
   return (
--- src/pages/MainLayout/MainLayout.tsx
+++ src/pages/MainLayout/MainLayout.tsx
@@ -1,21 +1,25 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { URL } from 'constants/api'
+import { useEffect } from 'react'
 import { SearchComponent } from 'components/SearchComponent/SearchComponent'
 import { Card, ICardProps } from 'components/Card/Card'
-import { Skeleton } from 'antd'
+import { Spin } from 'antd'
+import { useAppDispatch, useAppSelector } from 'hooks/reduxHook'
+import { setSneackers } from 'store/sneackers'
 import img from 'assets/img/1.jpg'
 
 import style from './MainLayout.module.scss'
 
 export const MainLayout = () => {
-  const url = 'https://60d62397943aa60017768e77.mockapi.io/items'
-  const [sneackers, setSneackers] = useState<ICardProps[] | null>(null)
+  const sneackers = useAppSelector((state) => state.sneackers.items)
+  const isLoaded = useAppSelector((state) => state.sneackers.isLoaded)
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
     const response = async () => {
       try {
-        const { data } = await axios.get(url)
-        setSneackers(data)
+        const { data } = await axios.get(`${URL}/items`)
+        dispatch(setSneackers(data))
       } catch (error) {
         if (error instanceof Error) {
           console.log(error.message)
@@ -23,7 +27,7 @@ export const MainLayout = () => {
       }
     }
     response()
-  }, [sneackers])
+  })
 
   return (
     <div className={style.container}>
@@ -31,15 +35,15 @@ export const MainLayout = () => {
         <h1 className={style.title}>All sneackers:</h1>
         <SearchComponent />
       </div>
-      {sneackers !== null ? (
+      {isLoaded ? (
         <div className={style.cards}>
           {sneackers.map((card: ICardProps) => (
             <Card key={card.id} id={card.id} title={card.title} price={card.price} imageUrl={img} />
           ))}
         </div>
       ) : (
-        <div>
-          <Skeleton />
+        <div className={style.loader}>
+          <Spin size='large' />
         </div>
       )}
     </div>
