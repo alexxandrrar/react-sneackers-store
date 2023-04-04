4f1e4277fc51d5b213080e7c13fee723319f7767
alexxandrrar <alexandroovnaa@gmail.com>
2023-04-04 13:57:28 +0300

    edited requests

--- server/src/sneakers/sneakers.controller.ts
+++ server/src/sneakers/sneakers.controller.ts
@@ -1,34 +1,59 @@
-import { sneakersDatabase } from './sneakers.database';
-import { ISneakers } from './sneakers.interface';
-import { Controller, Get, Param, Body, Put, HttpException, HttpStatus } from '@nestjs/common';
+import { sneakersDatabase } from "./sneakers.database";
+import { ISneakers } from "./sneakers.interface";
+import {
+  Controller,
+  Get,
+  Param,
+  Body,
+  Put,
+  InternalServerErrorException,
+} from "@nestjs/common";
 
-@Controller('sneakers')
+@Controller("sneakers")
 export class SneakersController {
   private sneakers: ISneakers[] = sneakersDatabase;
 
   @Get()
   getAllSneakers(): ISneakers[] {
-    return this.sneakers;
-  }
-
-  @Get(':id')
-  getSneakerById(@Param('id') id: string): ISneakers {
-    const sneaker = this.sneakers.find((s) => s.id === id);
-    if (!sneaker) {
-      throw new HttpException(`Sneaker with id ${id} not found.`, HttpStatus.NOT_FOUND);
+    try {
+      return this.sneakers;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-    return sneaker;
-
   }
 
-  @Put(':id')
+  @Get("favourite")
+  getAllFavouriteSneakers(): ISneakers[] {
+    try {
+      const favouriteSneakers = this.sneakers.filter((sneaker) => sneaker.isFavourite);
+      return favouriteSneakers;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  @Get("cart")
+  getAllCartSneakers(): ISneakers[] {
+    try {
+      const cartSneakers = this.sneakers.filter((sneaker) => sneaker.isInCart);
+      return cartSneakers;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  @Put(":id")
   updateSneaker(
-    @Param('id') id: string,
-    @Body() updatedSneaker: ISneakers,
+    @Param("id") id: string,
+    @Body() updatedSneaker: ISneakers
   ): ISneakers {
-    const index = this.sneakers.findIndex((sneaker) => sneaker.id === id);
-    const sneaker = { id, ...updatedSneaker };
-    this.sneakers[index] = sneaker;
-    return sneaker;
+    try {
+      const index = this.sneakers.findIndex((sneaker) => sneaker.id === id);
+      const sneaker = { id, ...updatedSneaker };
+      this.sneakers[index] = sneaker;
+      return sneaker;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
   }
 }
